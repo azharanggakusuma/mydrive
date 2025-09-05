edc68feda91511a42c38585938f1dab955732f23
azharanggakusuma <azharanggakusuma01@gmail.com>
2025-09-06 05:48:12 +0700

    feat: simplify application title in metadata for MyDrive

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "MyDrive - Simpan dan Bagikan File Anda dengan Mudah",
+  title: "MyDrive",
   description: "Simpan dan bagikan file Anda dengan mudah menggunakan MyDrive.",
 };
 
